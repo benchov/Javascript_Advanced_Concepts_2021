bbfaa7a1788a9be1355d2ced8ea5e659b0cf390d
benchov <viktor.mihalyi@yahoo.com>
2021-07-27 21:23:21 +0200

    feat(composition.js): Using Ramda library to handle multi parameter data consuming

--- functional programing/composition.js
+++ functional programing/composition.js
@@ -4,3 +4,21 @@ const absoluter = (num) => Math.abs(num);
 const multiplyBy3AndAbsolute = compose(multiplier, absoluter);
 
 console.log(multiplyBy3AndAbsolute(-7));
+
+// WORKING OPTION FOR COMPOSITION WITH RAMDA
+// Starting with deigned data object and send through the
+// chain of functions which can conusme the data.
+
+const getAndPass1 = (data) => {
+  return Object.assign({}, data, { value: data.value + 2 });
+};
+
+const getAndPass2 = (data) => {
+  return Object.assign({}, data, { error: "bad things happen" });
+};
+
+const getAndPass3 = (data) => {
+  return Object.assign({}, { value1: data.value + data.value2, error: "" });
+};
+
+R.compose(getAndPass3, getAndPass2, getAndPass1)({ value: 1, value2: 4 }); //=> 7
